a1b0f2cbf94be68ef249775a94c103931b0d0841
shagar <shagyfox133@gmail.com>
2021-03-23 12:11:14 +0530

    corrected error

--- public_html/src/thumb.js
+++ public_html/src/thumb.js
@@ -1,9 +1,10 @@
-function Enlargeimage(img,name,num) {
-  var ele = document.getElementsByClassName(name);
-  ele.classList.toggle('hidden');
+function Enlargeimage(img,x) {
+  var el = document.getElementsByClassName('img');
+  for (let i = 0; i < el.length; i++) {
+    el[i].classList.add('hidden')
+  }
+  document.getElementById('img_' + x).classList.remove('hidden');
   var src = img.getAttribute('src');
-
-  console.log(src)
   document.getElementById("imgbox").setAttribute('src', src).style.backgroundColor;
   
 }
