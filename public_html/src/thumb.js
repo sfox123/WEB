0b644de52a4c11818f0597ef0dd0cb0f66e46f1b
Sajivini <sajivini.2019922@iit.ac.lk>
2021-03-23 11:24:15 +0530

    Checked...

--- public_html/src/thumb.js
+++ public_html/src/thumb.js
@@ -1,6 +1,10 @@
-function Enlargeimage(img) {
+function Enlargeimage(img,name,num) {
+  var ele = document.getElementsByClassName(name);
+  ele.classList.toggle('hidden');
   var src = img.getAttribute('src');
+
   console.log(src)
   document.getElementById("imgbox").setAttribute('src', src).style.backgroundColor;
+  
 }
 
