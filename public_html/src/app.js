0782f216c125c9b23537c3c4cdad4d8300598c04
shagar <shagyfox133@gmail.com>
2021-03-24 20:11:07 +0530

    cart Logic step 1

--- public_html/src/app.js
+++ public_html/src/app.js
@@ -1,1 +1,19 @@
 console.log("Connected");
+
+const btn = document.getElementsByClassName('.item__link');
+
+document.addEventListener('click', (evt) => {
+    const el = evt.path[0].innerHTML;
+    let quantity = 0;
+    el == '+' ? quantity = evt.path[0].nextElementSibling.innerHTML : el == '-'
+        ? quantity = evt.path[0].previousElementSibling.innerHTML : null
+    if (el == '+') {
+        let count = parseInt(quantity);
+        count += 1;
+        evt.path[0].nextElementSibling.innerHTML = count;   
+    } else if(el == '-' && quantity > 1){
+        let count = parseInt(quantity);
+        count -= 1;
+        evt.path[0].previousElementSibling.innerHTML = count;   
+    }
+})
