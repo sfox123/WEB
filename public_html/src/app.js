0fe579cc89a04615a0e6c11653b2c9539f6bbfe9
shagar <shagyfox133@gmail.com>
2021-03-24 23:29:52 +0530

    backend cart logic

--- public_html/src/app.js
+++ public_html/src/app.js
@@ -25,15 +25,42 @@ function btnInt() {
         }
         else if (evt.path[0].classList[1] == 'item__btn') {
             let tmp = {
-                name: 'Hey',
-                count: evt.path[2].children[2].getElementsByClassName('item__count')[0].innerHTML
+                name: evt.path[2].getElementsByClassName('item__heading')[0].innerHTML,
+                count: evt.path[2].children[3].getElementsByClassName('item__count')[0].innerHTML,
+                price : evt.path[2].children[2].innerHTML.substring(3)
             }
             cart.items.push(tmp);
             cart.count += 1;
             cart_count.innerHTML = cart.count;
-            evt.path[2].children[2].getElementsByClassName('item__count')[0].innerHTML = 1;
-            console.log(cart)
+            evt.path[2].children[3].getElementsByClassName('item__count')[0].innerHTML = 1;
+            cartLogic();
         }
     });
 }
 
+function cartLogic() {
+    
+    const container = document.querySelector('#pop__container');
+    for (let item of cart.items) {
+        const box = document.createElement('div');
+        box.classList.add('pop__items');
+        
+        const h1 = document.createElement('h1');
+        h1.textContent = item.name;
+        h1.classList.add('pop__item__name');
+
+        const sum = document.createElement('p')
+        sum.textContent = 'Quantity : ' + item.count +" ||  Price : "+ item.price;
+        sum.classList.add('pop__item__quantity');
+        
+        const btn = document.createElement('button')
+        btn.textContent = 'Remove'
+        btn.classList.add('pop__item__remove');
+
+        container.appendChild(box);
+        box.appendChild(h1);
+        box.appendChild(sum);
+        box.appendChild(btn);
+    }
+    cart.items = [];
+}
